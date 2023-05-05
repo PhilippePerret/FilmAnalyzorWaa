c9a388acba9070fa6a9a73dd2fcf5374e7c30e60
PhilippePerret <philippe.perret@yahoo.fr>
2023-05-05 08:49:16 +0200

    Fixed #6 (duplication de la vidéo)

--- lib/required/app/Analyse.rb
+++ lib/required/app/Analyse.rb
@@ -74,12 +74,15 @@ class Analyse
   # @note
   #   - Si c'est le premier chargement de la journée, on fait un 
   #     backup.
+  #   - La méthode vérifie que la vidéo existe bien dans le dossier
+  #     me/Sites/FilmAnalyzor et, le cas échouant, la copie
   #   - Peut-être faudra-t-il découpe le texte, comme à l'enregistrement
   #     (mais je ne pense pas, vu ce que supportent les pages HTML)
   # 
   def self.load(data)
     analyse = new(data['path'])
     analyse.make_backup if analyse.backup_required?
+    analyse.check_video
     WAA.send(class:'Analyse',method:'onLoad',data:analyse.client_data)
   end
 
@@ -97,13 +100,33 @@ class Analyse
   #     - <nom film>.mp4      Fichiers du film
   def initialize(path)
     @path = path
+    @message = nil
   end
 
+  # @return [Hash] Les données à retourner au client
   def client_data
     {
       data:   data,
       texte:  File.read(texte_path.gsub(/"/,'__GUIL__')),
     }
+  end
+
+  def check_video
+    video_name = data['video']
+    video_path = File.join(path, video_name)
+    video_path_in_sites = File.join(Dir.home,'Sites','FilmAnalyzor',video_name)
+    return true if File.exist?(video_path_in_sites)
+    #
+    # La vidéo n'existe pas dans le dossier Sites. Si elle existe
+    # dans le dossier de l'analyse, on la copie
+    # 
+    if File.exist?(video_path)
+      puts "Copie de la vidéo, merci de patienter…".bleu
+      FileUtils.cp(video_path, video_path_in_sites)
+      puts "Vidéo copiée. Merci de votre patience.".vert
+    else
+      raise "La vidéo est malheureusement introuvable. Je ne peux pas ouvrir cette analyse."
+    end
   end
 
   def make_backup
